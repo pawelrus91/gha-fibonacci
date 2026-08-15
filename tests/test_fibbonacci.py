ceb2a4abf568972b0dfb460d47d6e3c003db5bef
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2026-08-15 21:57:16 +0200

    Implement fibonacci function with tests

--- tests/test_fibbonacci.py
+++ tests/test_fibbonacci.py
@@ -1,3 +1,4 @@
+import pytest
 import types
 
 from src.gha_fibonacci.fibonacci import fibonacci
@@ -5,3 +6,23 @@ from src.gha_fibonacci.fibonacci import fibonacci
 
 def test_function_exist() -> None:
     assert isinstance(fibonacci, types.FunctionType)
+
+
+testdata = [
+    (0, 0),
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 5),
+    (6, 8),
+    (9, 34),
+    (10, 55),
+    (11, 89),
+    (12, 144)
+]
+
+
+@pytest.mark.parametrize('n,excepted', testdata)
+def test_should_fibonacci_expected_value(n: int, excepted: int) -> None:
+    assert fibonacci(n) == excepted
